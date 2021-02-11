3d6e181f8d37a24c2f180b96ccfd065b333abb5c
docfern <docfern@gmail.com>
2021-02-10 22:47:39 -0800

    update homework

--- wallet/wallet.py
+++ wallet/wallet.py
@@ -1,105 +1,97 @@
 # Importing libraries
-import os
-import subprocess 
+import subprocess
 import json
-
-from dotenv import load_dotenv
-
 from constants import *
-from bit import Key, PrivateKey, PrivateKeyTestnet
-from bit.network import NetworkAPI
-from bit import *
+from dotenv import load_dotenv
+import os
 from web3 import Web3
-from eth_account import Account 
+from eth_account import Account
+from bit import wif_to_key
+load_dotenv()
+from bit import Key, PrivateKey, PrivateKeyTestnet
+from bit import *
+from bit.network import NetworkAPI
+from web3.auto.gethdev import w3
+from web3.middleware import geth_poa_middleware
 
+mnemonic = os.getenv('mnemonic')
 
-# Web3 connection and loading mnemonic
-# Nodes runing with POW
-w3 = Web3(Web3.HTTPProvider("http://127.0.0.1.8545"))
+mnemonic = os.getenv('mnemonic')
 
-# Loading EV
-load_dotenv
-
-# Loading Mnemonic EV and (set this mnemonic as an environment variable, and include the one you generated as a fallback using)
-mnemonic = os.getenv('MNEMONIC', "liar general lucky churn grit dolphin dismiss eyebrow click history maze round")
-print(mnemonic)
-
-# Functions to transact
-#1 create function to call the php file. As I am on windows I am not using standard ./derive but 
-def derive_wallets(mnemonic, coin, numderive):
-    """Use the subprocess library to call the php file script from Python"""
-    command = f'php ./hd-wallet-derive/hd-wallet-derive.php -g --mnemonic="{mnemonic}" --numderive="{numderive}" --coin="{coin}" --format=json' 
-    
+def derive_wallets(coin):
+    command = f'./derive -g --mnemonic="{mnemonic}" --cols=path,address,privkey,pubkey --format=json --coin="{coin}" --numderive= 2'
     p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
-    (output, err) = p.communicate()
-   
+    output, err = p.communicate()
+    p_status = p.wait()
     keys = json.loads(output)
-    return  keys
- 
+    return keys
+  
+coins = {
+    ETH: derive_wallets(ETH),
+    BTCTEST: derive_wallets(BTCTEST)
+}
+print(coins)
 
-# Create a coin object to hold child wallets
-coins = {"eth", "btc-test", "btc"}
-numderive = 3
-# Setting the dictionarry
-keys = {}
-for coin in coins:
-    keys[coin]= derive_wallets(os.getenv('mnemonic'), coin, numderive=3)
+INDEX = 0
 
-# Creating a private keys object
-eth_PrivateKey = keys["eth"][0]['privkey']
-btc_PrivateKey = keys['btc-test'][0]['privkey']
+#print(coins[BTCTEST][0]['privkey'])
 
+w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
+#print(w3.eth.blockNumber)
 
-print(json.dumps(eth_PrivateKey, indent=4, sort_keys=True))
-print(json.dumps(btc_PrivateKey, indent=4, sort_keys=True))
-print(json.dumps(keys, indent=4, sort_keys=True))
+#print(w3.eth.getBalance("0x21b8b9F8bB410C2CEcF92196812C7b83bAeb95C8"))
 
-#2 - convert private key string 
 def priv_key_to_account(coin, priv_key):
-    """Convert the privkey string in a child key to an account object that bit or web3.py can use to transact"""
     if coin == ETH:
         return Account.privateKeyToAccount(priv_key)
-    if coin == BTCTEST:
+    elif coin == BTCTEST:
         return PrivateKeyTestnet(priv_key)
-    
-eth_acc = priv_key_to_account(ETH,eth_PrivateKey)
-btc_acc = priv_key_to_account(BTCTEST,btc_PrivateKey)
 
-#3 create transaction with necesseary metadata
 
-def create_trx(coin, account, recipient, amount):
-    """create the raw, unsigned transaction that contains all metadata needed to transact"""
-    global trx_data
-    if coin ==ETH:
+def create_tx(coin, account, to, amount):
+    if coin == ETH:
         gasEstimate = w3.eth.estimateGas(
-            {"from": account.address, "to": recipient, "value": amount}
+            {"from": account.address, "to": to, "value": amount}
         )
-        trx_data = {
-            "to": recipient,
+        return {
             "from": account.address,
+            "to": to,
             "value": amount,
             "gasPrice": w3.eth.gasPrice,
             "gas": gasEstimate,
-            "nonce": w3.eth.getTransactionCount(account.address)
+            "nonce": w3.eth.getTransactionCount(account.address),
+            #"chainID": web3.eth.chainId
         }
-        return trx_data
+    elif coin == BTCTEST:
+        return PrivateKeyTestnet.prepare_transaction(account.address, [(to, amount, BTC)])
 
-    if coin == BTCTEST:
-        return PrivateKeyTestnet.prepare_transaction(account.address, [(recipient, amount, BTC)]) 
+def send_tx(coin, account, to, amount):
+    tx = create_tx(coin, account, to, amount)
+    signed_tx = account.sign_transaction(tx)
+    if coin == ETH: 
+        return w3.eth.sendRawTransaction(signed.rawTransaction)
+    elif coin == BTCTEST: 
+        return NetworkAPI.broadcast_tx_testnet(signed_tx) 
 
-#4 create, sign, send the transaction 
+Account_one = priv_key_to_account(BTCTEST, coins[BTCTEST][0]['privkey'])
 
-def send_trx(coin, account, recipient, amount):
-    """call create_trx, sign the transaction, then send it to the designated network"""
-    if coin == "eth": 
-        trx_eth = create_trx(coin,account, recipient, amount)
-        sign = account.signTransaction(trx_eth)
-        result = w3.eth.sendRawTransaction(sign.rawTransaction)
-        print(result.hex())
-        return result.hex()
-    else:
-        trx_btctest= create_trx(coin,account,recipient,amount)
-        sign_trx_btctest = account.sign_transaction(trx_btctest)
-        from bit.network import NetworkAPI
-        NetworkAPI.broadcast_tx_testnet(sign_trx_btctest)       
-        return sign_trx_btctest
+#print(Account_one)
+#print(coins[BTCTEST][0]['address'])
+print(coins[BTCTEST][1]['privkey'])
+
+# insert private key here
+key = wif_to_key(os.getenv('keys'))
+key2 = wif_to_key(os.getenv('keys2'))
+
+address_two= coins[BTCTEST][1]['address']
+
+#send_tx(BTCTEST, Account_one, address_two, 0.002)
+
+print(key.get_balance("btc"))
+print(key.balance_as("usd"))
+#print(key.get_transactions())
+#print(key.get_unspents())
+print(key2.get_balance("btc"))
+print(key2.balance_as("usd"))
+#print(key2.get_transactions())
+#print(key2.get_unspents())
